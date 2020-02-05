b7f77f202eae7a084ec91116c2da901cf0ab9fc0
Evgeny Fomin <fominok@hotmail.com>
2020-02-05 16:09:35 +0300

    test other storage movements

--- src/selections/storage/tests/movements.rs
+++ src/selections/storage/tests/movements.rs
@@ -153,3 +153,83 @@ fn test_move_left_intersection() {
 
     assert_eq!(selections_vec, selections_reference_vec);
 }
+
+#[test]
+fn test_move_right_intersection() {
+    let mut line_length = HashMap::new();
+    line_length.insert(1, 9);
+    line_length.insert(2, 9);
+    line_length.insert(3, 9);
+    let mut storage =
+        gen_storage_from_tuples(&vec![(1, 3, 1, 5, true), (2, 5, 2, 7, true)], &line_length);
+
+    storage.move_right(9, true);
+
+    let selections_vec: Vec<Selection> = storage
+        .selections_tree
+        .iter()
+        .map(|x| x.0.clone())
+        .collect();
+    let selections_reference_vec = vec![Selection::new_quick(1, 3, 3, 7, CursorDirection::Forward)];
+
+    assert_eq!(selections_vec, selections_reference_vec);
+}
+
+#[test]
+fn test_move_down_intersection() {
+    let mut line_length = HashMap::new();
+    line_length.insert(1, 9);
+    line_length.insert(2, 9);
+    line_length.insert(3, 9);
+    let mut storage =
+        gen_storage_from_tuples(&vec![(1, 3, 1, 5, true), (2, 5, 2, 7, true)], &line_length);
+
+    storage.move_down(9, true);
+
+    let selections_vec: Vec<Selection> = storage
+        .selections_tree
+        .iter()
+        .map(|x| x.0.clone())
+        .collect();
+    let selections_reference_vec = vec![Selection::new_quick(1, 3, 3, 7, CursorDirection::Forward)];
+
+    assert_eq!(selections_vec, selections_reference_vec);
+}
+
+#[test]
+fn test_move_up_intersection() {
+    let mut line_length = HashMap::new();
+    line_length.insert(1, 9);
+    line_length.insert(2, 9);
+    line_length.insert(3, 9);
+    line_length.insert(4, 9);
+    let mut storage =
+        gen_storage_from_tuples(&vec![(3, 3, 3, 5, true), (4, 5, 4, 7, true)], &line_length);
+
+    storage.move_up(1, true);
+
+    let selections_vec: Vec<Selection> = storage
+        .selections_tree
+        .iter()
+        .map(|x| x.0.clone())
+        .collect();
+    let selections_reference_vec = vec![
+        Selection::new_quick(2, 5, 3, 3, CursorDirection::Backward),
+        Selection::new_quick(3, 7, 4, 5, CursorDirection::Backward),
+    ];
+
+    assert_eq!(selections_vec, selections_reference_vec);
+
+    storage.move_up(1, true);
+
+    let selections_vec: Vec<Selection> = storage
+        .selections_tree
+        .iter()
+        .map(|x| x.0.clone())
+        .collect();
+
+    let selections_reference_vec =
+        vec![Selection::new_quick(1, 5, 4, 5, CursorDirection::Backward)];
+
+    assert_eq!(selections_vec, selections_reference_vec);
+}
