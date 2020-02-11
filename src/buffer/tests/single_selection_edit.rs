1f60779bfbbbd842864d985225c68058952f5d0b
Evgeny Fomin <fominok@hotmail.com>
2020-02-12 00:24:42 +0300

    improve test for simple insertion case

--- src/buffer/tests/single_selection_edit.rs
+++ src/buffer/tests/single_selection_edit.rs
@@ -10,3 +10,25 @@ fn insert_some_characters() {
     reference_buffer.insert_for_test(1, 31, "awesome crate named ");
     assert_eq!(buffer, reference_buffer);
 }
+
+#[test]
+fn insert_some_characters_before_selection() {
+    let mut buffer = load_buffer();
+    buffer.move_right(40, false);
+    buffer.move_left(10, true);
+    buffer.insert("awesome crate named ");
+    let mut reference_buffer = load_buffer_with_selections(&vec![(1, 51, 1, 61, false)]);
+    reference_buffer.insert_for_test(1, 31, "awesome crate named ");
+    assert_eq!(buffer, reference_buffer);
+}
+
+#[test]
+fn insert_some_characters_after_selection() {
+    let mut buffer = load_buffer();
+    buffer.move_right(20, false);
+    buffer.move_right(10, true);
+    buffer.insert("awesome crate named ");
+    let mut reference_buffer = load_buffer_with_selections(&vec![(1, 21, 1, 51, true)]);
+    reference_buffer.insert_for_test(1, 31, "awesome crate named ");
+    assert_eq!(buffer, reference_buffer);
+}
