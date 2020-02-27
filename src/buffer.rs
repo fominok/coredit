7e44fdf563f1b0729f2f45d747cda8440e49f9e2
Evgeny Fomin <fominok@hotmail.com>
2020-02-27 13:31:40 +0300

    fix partial deletion for crlf

--- src/buffer.rs
+++ src/buffer.rs
@@ -50,7 +50,7 @@ impl Buffer {
     ///
     /// let file = File::open("test_data/one_liner.txt").unwrap();
     /// let mut buffer = Buffer::from_reader(file).unwrap();
-    /// assert_eq!(buffer.to_string(), "That was easy\n".to_string());
+    /// assert!(buffer.to_string().starts_with("That was easy"));
     /// ```
     pub fn from_reader<R: io::Read>(reader: R) -> Result<Self> {
         let rope = Rope::from_reader(reader).context(CreateFromReader)?;
@@ -105,7 +105,14 @@ impl Buffer {
         to_col: usize,
     ) {
         let ch_from = self.rope.line_to_char(from_line - 1) + from_col - 1;
-        let ch_to = self.rope.line_to_char(to_line - 1) + to_col - 1;
+        let ch_to: usize = {
+            let to_line_length = self.rope.length(to_line);
+            if Into::<usize>::into(to_col) == to_line_length.unwrap() {
+                self.rope.line_to_char(to_line) - 1
+            } else {
+                self.rope.line_to_char(to_line - 1) + to_col - 1
+            }
+        };
         self.rope.remove(ch_from..=ch_to);
     }
 
@@ -149,8 +156,14 @@ impl Buffer {
 
             let from_ch: usize =
                 self.rope.line_to_char(from_line - 1) + Into::<usize>::into(from.col) - 1;
-            let to_ch: usize =
-                self.rope.line_to_char(to_line - 1) + Into::<usize>::into(to.col) - 1;
+            let to_ch: usize = {
+                let to_line_length = self.rope.length(to_line);
+                if Into::<usize>::into(to.col) == to_line_length.unwrap() {
+                    self.rope.line_to_char(to_line).saturating_sub(1)
+                } else {
+                    self.rope.line_to_char(to_line - 1) + Into::<usize>::into(to.col) - 1
+                }
+            };
 
             current_selection = self.selection_storage.get_first_before(&s);
             self.selection_storage.apply_delete(s, &self.rope);
